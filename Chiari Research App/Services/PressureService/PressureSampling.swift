02697be292c5f8bb3fd84c7662d7361fb87126c4
GeorgeJMattis <mattis.g@northeastern.edu>
2026-04-18 23:53:34 -0400

    fixed async func

--- Chiari Research App/Services/PressureService/PressureSampling.swift
+++ Chiari Research App/Services/PressureService/PressureSampling.swift
@@ -2,24 +2,16 @@
 /**
     Has functions to start and stop pressure sampling, as well as a function to take a single read of the pressure sensor. The actual implementation of these functions will depend on the specific hardware and software being used for pressure sensing, but this class serves as a template for how to structure the code for pressure sampling in the app.
  */
-import CMMotionManager
 
 protocol PressureSampling {
 
 
 
-    func startSampling() async{
-        
-        
-    }
-
-    func stopSampling() async {
-        
-    }
-
-    func singleRead() async {
+    func startSampling() async
     
-    }
+    func stopSampling() async
+    
+    func singleRead() async
 
 }
 
